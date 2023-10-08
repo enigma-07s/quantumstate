8cee02f2a2325c56f29ce4d24bbad38c81ef751d
enigma-07s <89199101+enigma-07s@users.noreply.github.com>
2023-10-08 00:45:15 +0000

    ..

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
